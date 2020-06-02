20feb889bbd25b4eb3b80e27984b3692e1f8b314
jonifcup <53014087+jonifcup@users.noreply.github.com>
2020-06-02 19:12:01 +0100

    Update interface.h

--- Alinea B Final/interface.h
+++ Alinea B Final/interface.h
@@ -33,19 +33,19 @@ void printMapByLevel(Map *root);
 /* Imprime a informação relevante a celula.Função so serve para fazer debug*/
 void printCell(Cell *cell);
 
-/* Imprime a informação de quantos navios estão ativos para cada tipo de barco*/
+/* Imprime a informação de quantos barcos estão ativos para cada tipo de barco*/
 void informationShipRemaining(Ship *arrShip);
 
 /* Imprime a informação realtiva a cada tipo de barco como tamanho, active etc*/
 void printAllShipInfo(Ship *arrShip);
 
-/* Imprime a informação realativa a um tipo de navio*/
+/* Imprime a informação realativa a um tipo de barco*/
 void printShipInfo(Ship *ship);
 
 /* Imprime toda a informação e a forma de todos os tipos de barco*/
 void printAllShip(Ship *arrShip);
 
-/* Imprime A informação e a forma relativamente a um tipo de navio*/
+/* Imprime A informação e a forma relativamente a um tipo de barco*/
 void printShip(Ship *ship);
 
 /* Imprime a forma do barco numa mapa 5x5*/
